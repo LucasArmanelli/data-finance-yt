f0f370b8a1561e1cb8a4fc5651e4732b999c2720
Lucas Armanelli <luc.armanelli@gmail.com>
2023-07-26 14:06:28 -0300

    release

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Laptop from '../assets/laptop.jpg'
+
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white p-16 px-4'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+        <img className='w-[500px] mx-auto my-auto' src={Laptop} alt="/" />
+        <div className='flex flex-col justify-center'>
+          <p className='text-[#00df9a] font-bold'>DASHBOARD</p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Controle seus dados através de um painel de controle</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Perferendis quos dolore aut quia maxime labore dolorem voluptates illum,
+            nobis numquam ducimus quam corporis, officia vitae! Laborum quo odio exercitationem fuga.
+          </p>
+          <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Começar Agora</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Analytics
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2'>DESENVOLVIDO COM REACT JS E TAILWIND CSS</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>As vantagens do React.</h1>
+      <div className='flex justify-center items-center'>
+        <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Rápido e Flexível para</p>
+        <Typed
+        className='md:text-5xl sm:text-4xl text-xl font-bold pl-2'
+          strings={['BTB', 'BTC', 'SASS']}
+          typeSpeed={120}
+          backSpeed={140}
+          loop
+        />
+      </div>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>Tenha total controle de suas plataformas BTB, BTC, & SASS.</p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Saiba Mais</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
